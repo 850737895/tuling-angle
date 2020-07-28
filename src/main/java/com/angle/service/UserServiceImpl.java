b30c3ad69494a7e00756f156a6a52334bf69ff2a
sys_smlz <sys_smlz@qq.com>
2020-07-28 17:21:40 +0800

    angle-smlz也修改了insertUser方法

--- src/main/java/com/angle/service/UserServiceImpl.java
+++ src/main/java/com/angle/service/UserServiceImpl.java
@@ -1,5 +1,7 @@
 package com.angle.service;
 
+import com.angle.entity.User;
+
 /**
  * [来个全套]
  *
@@ -9,4 +11,13 @@ package com.angle.service;
  * @date 2020/7/28 16:45
  */
 public class UserServiceImpl {
+
+    public void insertUser(User user) {
+
+        System.out.println("模拟执行insertUser操作");
+
+        if(null != user) {
+            throw new IllegalArgumentException("参数为空");
+        }
+    }
 }
